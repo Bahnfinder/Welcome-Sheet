e9c4fe66877169c1dfe0efd00b72d35257f45ab2
Victor Lobe <32442832+victorlobe@users.noreply.github.com>
2025-04-18 03:58:16 +0200

    Fixes

--- Sources/WelcomeSheet/Views/WelcomeSheetPageView.swift
+++ Sources/WelcomeSheet/Views/WelcomeSheetPageView.swift
@@ -1,185 +1,177 @@
 //
-//  WelcomeSheetPageView.swift
+//  WelcomeSheets.swift
+//  Bahnfinder
 //
-//
-//  Created by Jakub Florek on 27/11/2021.
+//  Created by Victor Lobe on 04.03.24.
 //
 
-import SwiftUI
+import Foundation
+import WelcomeSheet
+import UIKit
 
-struct WelcomeSheetPageView: View {
-    let page: WelcomeSheetPage
-    let restPages: [WelcomeSheetPage]
-    let onDismiss: () -> Void
-    
-    let isiPad = UIDevice.current.userInterfaceIdiom == .pad
-    
-    @State private var optionalView: AnyView?
-    
-    var body: some View {
-        VStack(spacing: 0) {
-            ScrollView {
-                VStack(spacing: iPhoneDimensions.spacing) {
-                    if let headerImage = page.headerImage {
-                        Image(uiImage: headerImage)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(page.accentColor ?? Color.accentColor)
-                            .padding(.top, iPhoneDimensions.topPadding - (isiPad ? 15 : 0))
-                            .padding(.horizontal, 20)
-                    }
-                    
-                    HStack {
-                        Spacer()
-                        
-                        if #available(iOS 15.0, *) {
-                            page.title.toText()
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .lineSpacing(8)
-                                .multilineTextAlignment(.center)
-                                .padding(.top, page.headerImage == nil ? iPhoneDimensions.topPadding - (isiPad ? 15 : 0) : 10)
-                                .fixedSize(horizontal: false, vertical: true)
-                                .accessibilityHeading(.h1)
-                        } else {
-                            page.title.toText()
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .lineSpacing(8)
-                                .multilineTextAlignment(.center)
-                                .padding(.top, page.headerImage == nil ? iPhoneDimensions.topPadding - (isiPad ? 15 : 0) : 10)
-                                .fixedSize(horizontal: false, vertical: true)
-                        }
-                        
-                        Spacer()
-                    }
-                    
-                    VStack(alignment: .midIcons, spacing: 30) {
-                        ForEach(page.rows) { row in
-                            HStack(spacing: 17.5) {
-                                row.image
-                                    .renderingMode(.template)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .foregroundColor(row.accentColor ?? Color.accentColor)
-                                    .frame(width: 37, height: 37)
-                                    .alignmentGuide(.midIcons) { d in d[HorizontalAlignment.center] }
-                                    .accessibility(hidden: true)
-                                
-                                VStack(alignment: .leading, spacing: 2) {
-                                    Text(row.title)
-                                        .font(.headline)
-                                        .lineLimit(2)
-                                        .fixedSize(horizontal: false, vertical: true)
-                                    
-                                    Text(row.content)
-                                        .font(.subheadline)
-                                        .foregroundColor(.secondary)
-                                        .fixedSize(horizontal: false, vertical: true)
-                                }
-                                
-                                Spacer()
-                            }
-                            .padding(.horizontal, 20 + iPhoneDimensions.horizontalPaddingAddend)
-                            .accessibilityElement(children: .combine)
-                        }
-                    }
-                }
-                .padding(.horizontal)
-                .padding(.horizontal, isiPad ? 45 : 0)
-            }
-            .scrollOnlyOnOverflow()
-            
-            VStack(spacing: 5) {
-                if page.isShowingOptionalButton {
-                    if let optionalButtonTitle = page.optionalButtonTitle {
-                        Button(optionalButtonTitle) {
-                            if let view = page.optionalButtonView {
-                                optionalView = view
-                            } else if let action = page.optionalButtonAction {
-                                action()
-                            } else if let url = page.optionalButtonURL {
-                                UIApplication.shared.open(url)
-                            }
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                        .font(Font.headline.weight(.medium))
-                        .foregroundColor(page.accentColor ?? Color.accentColor)
-                        .padding(.top)
-                    }
-                }
-                
-                if let nextPage = restPages.first {
-                    NavigationLink {
-                        WelcomeSheetPageView(page: nextPage, restPages: restPages.filter({ $0.id != nextPage.id }), onDismiss: onDismiss)
-                    } label: {
-                        ZStack {
-                            page.accentColor ?? Color.accentColor
-                            
-                            Text(page.mainButtonTitle)
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding()
-                        }
-                        .frame(width: isiPad ? iPadSheetDimensions.width / 1.7 : nil)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
-                    }
-                    .buttonStyle(PlainButtonStyle())
-                    .padding(.horizontal, 10)
-                    .padding(.top)
-                } else {
-                    Button {
-                        onDismiss()
-                    } label: {
-                        ZStack {
-                            page.accentColor ?? Color.accentColor
-                            
-                            Text(page.mainButtonTitle)
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding()
-                        }
-                        .frame(width: isiPad ? iPadSheetDimensions.width / 1.7 : nil)
-                        .fixedSize(horizontal: false, vertical: true)
-                        .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
-                    }
-                    .buttonStyle(PlainButtonStyle())
-                    .padding(.horizontal, 10)
-                    .padding(.top)
-                }
-            }
-            .padding(.horizontal, 15 + iPhoneDimensions.horizontalPaddingAddend)
-            .padding(.bottom, 60)
-            .sheet(item: $optionalView) { view in
-                view.edgesIgnoringSafeArea(.all)
-            }
-        }
-        .background(
-            page.backgroundColor.edgesIgnoringSafeArea(.all)
-        )
-        .edgesIgnoringSafeArea(.top)
-    }
-}
 
-extension NSAttributedString {
-    func toText() -> Text {
-        var result = Text("")
-        self.enumerateAttributes(in: NSRange(location: 0, length: self.length), options: []) { attributes, range, _ in
-            let substring = self.string[Range(range, in: self.string)!]
-            var text = Text(substring)
-            
-            if let font = attributes[.font] as? UIFont {
-                text = text.font(Font(font))
+
+let welcomeSheet3 = [
+    WelcomeSheetPage(title: NSAttributedString(string: "Neues in Bahnfinder 3"), rows: [
+        WelcomeSheetPageRow(imageSystemName: "speaker.wave.2.bubble.left",
+                            title: "Haltestellen vorlesen",
+                            content: "Mit \"Haltestellen vorlesen\" werden die nächsten Haltestellen nicht nur angesagt, sondern du erhältst auch eine Erinnerung, wenn es Zeit ist auszusteigen."),
+        WelcomeSheetPageRow(imageSystemName: "calendar.day.timeline.leading",
+                            title: "Fahrtverlauf in Sekunden",
+                            content: "Verfolge dein Fahrzeug sekundengenau mit einem noch detaillierteren Fahrtverlauf."),
+        WelcomeSheetPageRow(imageSystemName: "rectangle.grid.1x2",
+                            title: "Kompakte Abfahrten",
+                            content: "Neues auswählbares Design für Abfahrten."),
+        WelcomeSheetPageRow(imageSystemName: "arrow.triangle.swap",
+                            title: "Mehr Daten im Verbindungsdetail",
+                            content: "Mit Bahnfinder 3 werden dir detailliertere Informationen über deine Fahrt gezeigt."),
+        WelcomeSheetPageRow(imageSystemName: "app.dashed",
+                            title: "Neue App Icons",
+                            content: "8 neue App Icons machen Bahnfinder noch persönlicher."),
+        WelcomeSheetPageRow(imageSystemName: "ellipsis",
+                            title: "Über 30 weitere Verbesserungen",
+                            content: "Die meisten davon, machen Bahnfinder im Hintergrund noch schneller und zuverlässiger.")],
+                     mainButtonTitle: "Fortfahren"
+                    )]
+
+
+let welcomeSheet4 = [
+    WelcomeSheetPage(title: NSAttributedString(string: "Neues in Bahnfinder 4"), rows: [
+        
+        WelcomeSheetPageRow(imageSystemName: "bell",
+                            title: "Erinnerungen",
+                            content: "Lasse dich rechtzeitig an Abfahrten erinnern."),
+        
+        WelcomeSheetPageRow(imageSystemName: "clock.arrow.circlepath",
+                            title: "Neuer Synchronisieren Tab",
+                            content: "iCloud“ heisst jetzt „Synchronisieren“. Du kannst sowohl iCloud als auch Lokale Backups erstellen und wiederherstellen und diese sogar in andere Apps exportieren."),
+        
+        WelcomeSheetPageRow(imageSystemName: "hand.tap",
+                            title: "3D-Touch Menü für Haltestellen",
+                            content: "Zugriff auf zusätzliche Optionen für Haltestellen im Fahrtverlauf mit einem neuen 3D-Touch-Menü."),
+        
+        WelcomeSheetPageRow(imageSystemName: "arrow.triangle.swap",
+                            title: "Verbindungen direkt aus dem Fahrtverlauf suchen",
+                            content: "Du kannst über das 3D Touch Menü eine bestimmte Haltestelle als Ziel auswählen und schnell eine Verbindungssuche starten."),
+        
+        WelcomeSheetPageRow(imageSystemName: "square.split.1x2",
+                            title: "Split Screen",
+                            content: "Nutze Bahnfinder jetzt im Split-Screen-Modus für noch effizienteres Multitasking.."),
+        
+        WelcomeSheetPageRow(imageSystemName: "ellipsis",
+                            title: "Über 20 weitere Verbesserungen",
+                            content: "Die meisten davon, machen Bahnfinder im Hintergrund noch schneller und zuverlässiger.")],
+                     
+                     mainButtonTitle: "Fortfahren"
+                    )]
+
+
+let welcomeSheet5 = [
+    WelcomeSheetPage(title: NSMutableAttributedString(string: "Neues in Bahnfinder 5"), rows: [
+        
+        WelcomeSheetPageRow(imageSystemName: "123.rectangle.fill",
+                            title: "Liniennummer in Verbindungsliste",
+                            content: "Du kannst jetzt aussuchen, ob du die Liniennummer oder das Fahrzeugsymbol in der Verbindungsliste sehen möchtest."),
+        
+        WelcomeSheetPageRow(imageSystemName: "calendar.day.timeline.left",
+                            title: "Verbesserungen im Fahrtverlauf",
+                            content: "Im Fahrtverlauf werden jetzt weitere Informationen zu deinem Fahrzeug angezeigt. Du kannst genau sehen, von welchem Gleis dein Fahrzeug fährt oder welche Haltestellen ausfallen."),
+        
+        WelcomeSheetPageRow(imageSystemName: "train.side.rear.car",
+                            title: "Wagenreihung",
+                            content: "Du kannst dir jetzt über den Fahrtverlauf die Wagenreihung von ICEs ansehen."),
+        
+        WelcomeSheetPageRow(imageSystemName: "person.3.sequence.fill",
+                            title: "Auslastung",
+                            content: "Bei bestimmten Fahrzeugen siehst du, wie ausgelastet deine Fahrt sein wird."),
+        
+        WelcomeSheetPageRow(imageSystemName: "ticket.fill",
+                            title: "Fahrpreise",
+                            content: "Im Verbindungsdetail kannst du dir die aktuellen Ticketpreise für deine Fahrt ansehen."),
+        
+        WelcomeSheetPageRow(imageSystemName: "bell.fill",
+                            title: "Erinnerungen",
+                            content: "Erinnerungen sind jetzt auch für Verbindungen verfügbar."),
+        
+        
+        WelcomeSheetPageRow(imageSystemName: {
+            if #available(iOS 16, *) {
+                return "speaker.wave.2.bubble.left.fill"
+            } else {
+                return "speaker.wave.2.circle"
             }
-            if let foregroundColor = attributes[.foregroundColor] as? UIColor {
-                text = text.foregroundColor(Color(foregroundColor))
-            }
-            // Add more attributes as needed
+        }(), title: "Haltestellen ankündigen",
+                            content: "Haltestellen vorlesen heisst jetzt Haltestellen ankündigen. Du kannst dir die nächsten Haltestellen jetzt auch als Push Mitteilung anzeigen lassen, falls du mal keine Kopfhörer dabei hast."),
+        
+        WelcomeSheetPageRow(imageSystemName: "paintpalette.fill",
+                            title: "Neue Design Einstellungen",
+                            content: "Mit den neuen Design Einstellungen kannst du dein gewünschtes Design noch einfacher und übersichtlicher ändern."),
+        
+        WelcomeSheetPageRow(imageSystemName: "line.3.horizontal.decrease.circle.fill",
+                            title: "Wichtige Haltestellen filtern",
+                            content: "Du kannst in der Suche auswählen, ob du nur die wichtigsten Haltestellen in deiner Nähe sehen möchtest. Diese basieren auf deinen Favoriten, benutzten und besuchten Haltestellen.")
+        
+    ],
+                     mainButtonTitle: "Fortfahren"
+                    )]
+
+
+
+
+let stationSpeechIntro = [
+    WelcomeSheetPage(
+        title: NSMutableAttributedString(string: "Haltestellen ankündigen"),
+        rows: [
+            WelcomeSheetPageRow(
+                imageSystemName: {
+                    if #available(iOS 16, *) {
+                        return "speaker.wave.2.bubble.left.fill"
+                    } else {
+                        return "speaker.wave.2.circle"
+                    }
+                }(),
+                accentColor: BahnfinderColorGreen.toColor,
+                title: "Haltestellen vorlesen lassen",
+                content: "Lasse dir die nächsten Haltestellen vorlesen, um keine Haltestelle mehr zu verpassen."
+            ),
             
-            result = result + text
-        }
-        return result
-    }
-}
+            WelcomeSheetPageRow(
+                imageSystemName: "bell.badge.fill",
+                accentColor: BahnfinderColorBase.toColor,
+                title: "Push Mitteilungen",
+                content: "Du kannst dir die Haltestellen auch per Push Benachrichtigung anzeigen lassen, falls du mal keine Kopfhörer dabei hast."
+            ),
+            
+            WelcomeSheetPageRow(
+                imageSystemName: {
+                    if #available(iOS 16, *) {
+                        return "figure.walk.departure"
+                    } else {
+                        return "figure.wave"
+                    }
+                }(),
+                accentColor: BahnfinderColorBlue.toColor,
+                title: "Benachrichtigung beim Aussteigen",
+                content: "Du wirst rechtzeitig erinnert, wenn du aussteigen musst – wähle dazu einfach deine Zielhaltestelle aus."
+            )
+        ],
+        accentUIColor: BahnfinderColorBase,
+        mainButtonTitle: "Aktivieren"
+    )
+]
+
+let autoFollowIntro = [
+    WelcomeSheetPage(
+        title: NSMutableAttributedString(string: "Automatisch folgen"),
+        rows: [
+            WelcomeSheetPageRow(
+                imageSystemName: "dot.circle.viewfinder",
+                accentColor: UIColor.systemYellow.toColor,
+                title: "", content: "Mit der Funktion \"Automatisch folgen\" werden dir jederzeit automatisch die Abfahrten für die nächste Haltestelle angezeigt, während du dich fortbewegst. Sobald die Funktion aktiviert ist, erscheint oben links ein Symbol, mit dem du die Funktion ganz einfach wieder deaktivieren kannst."
+            )
+        ],
+        accentUIColor: .systemYellow,
+        mainButtonTitle: "Aktivieren",
+        headerImage: UIImage(systemName: "location.circle.fill")?.applyingSymbolConfiguration(.init(pointSize: 80, weight: .regular))
+    )
+]
